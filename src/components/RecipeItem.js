73c96d4e6ba857dbf6f3eaf93c65fefcee300bab
Niks156 <nayangarg408@gmail.com>
2021-12-04 20:17:37 +0530

    Css Applied

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -1,42 +1,38 @@
-import React from 'react'
-import IngredientList from './IngredientList';
+import React from "react";
+import IngredientList from "./IngredientList";
 
 export default function RecipeItem(props) {
-    const {
-        name,
-        servings,
-        cookTime,
-        instructions,
-        ingredients
-    } = props;
-    //Object Destructuring.
-    return (
+  const { name, servings, cookTime, instructions, ingredients } = props;
+  //Object Destructuring.
+  return (
+    <div className="recipe">
+      <div className="recipe__header">
+        <h3 className="recipe__title">{name} </h3>
         <div>
-            <div>
-                <h3>{name} </h3>
-                <div>
-                    <button>Edit</button>
-                    <button>Delete</button>
-                </div>
-            </div>
-            <div>
-                <span>Cook time:</span>
-                <span>{cookTime}</span>
-            </div>
-            <div>
-                <span>Servings:</span>
-                <span>{servings}</span>
-            </div>
-            <div>
-                <span>Instructions:</span>
-                <div>{instructions} </div>
-            </div>
-            <div>
-                <span>Ingredients:</span>
-                <div>
-                    <IngredientList ingredients={ingredients} /> 
-                </div>
-            </div>
+          <button className="btn btn--primary mr-1">Edit</button>
+          <button className="btn btn--danger">Delete</button>
         </div>
-    )
+      </div>
+      <div className="recipe__row">
+        <span className="recipe__label">Cook Time:</span>
+        <span className="recipe__value">{cookTime}</span>
+      </div>
+      <div className="recipe__row">
+        <span className="recipe__label">Servings:</span>
+        <span className="recipe__value">{servings}</span>
+      </div>
+      <div className="recipe__row">
+        <span className="recipe__label">Instructions:</span>
+        <div className="recipe__value recipe__instructions recipe__value--indented">
+          {instructions}
+        </div>
+      </div>
+      <div className="recipe__row">
+        <span className="recipe__label">Ingredients:</span>
+        <div className="recipe__value recipe__value--indented">
+          <IngredientList ingredients={ingredients} />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -3,13 +3,15 @@ import RecipeItem from "./RecipeItem";
 
 export default function RecipeList({ recipes }) {
     return (
-        <>
+        <div className="recipe-list">
         <div>
         {recipes.map(recipe => {
             return <RecipeItem key={recipe.id} {...recipe} />
         })}
         </div>
-        <button>AddRecipe</button>
-        </>
+        <div className="recipe-list__add-recipe-btn-container">
+        <button className="btn btn--primary">AddRecipe</button>
+        </div>
+        </div>
     )
 }
